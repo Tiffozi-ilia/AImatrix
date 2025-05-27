bf8ae238f522895b7603c0fdaedbd1d812502c54
Tiffozi-ilia <110051189+Tiffozi-ilia@users.noreply.github.com>
2025-05-27 09:05:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routes import json_export, md_full, md_clean, zip_export, csv_export, excel_export, opml_export, mm_export, xmind_export
+from routes import json_export, md_full, md_clean, zip_export, csv_export, excel_export, opml_export, mm_export, xmind_export, xmind_diff
 
 app = FastAPI()
 
@@ -12,3 +12,4 @@ app.include_router(excel_export.router)
 app.include_router(opml_export.router)
 app.include_router(mm_export.router)
 app.include_router(xmind_export.router)
+app.include_router(xmind_diff.router)
