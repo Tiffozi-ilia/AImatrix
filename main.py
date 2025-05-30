b86a25fc54c9d32e236b3dcbd3d824e047392092
Tiffozi-ilia <110051189+Tiffozi-ilia@users.noreply.github.com>
2025-05-30 18:50:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routes import json_export, md_full, md_clean, zip_export, csv_export, excel_export, opml_export, mm_export, xmind_export, xmind_diff, xmind_updated, xmind_delete, pyrus_mapping, pyrus_upload, xmind_procedures
+from routes import json_export, md_full, md_clean, zip_export, csv_export, excel_export, opml_export, mm_export, xmind_export, pyrus_upload, xmind_procedures
 
 
 app = FastAPI()
