568ef1ae8bddccfdaefde881bf7d7f254a8584a7
Tiffozi-ilia <110051189+Tiffozi-ilia@users.noreply.github.com>
2025-06-04 14:54:15 +0300

    Update call_pyrus_upload.py

--- routes/call_pyrus_upload.py
+++ routes/call_pyrus_upload.py
@@ -1,6 +1,5 @@
 import requests
-import json
-from fastapi import FastAPI, APIRouter
+from fastapi import APIRouter
 from pydantic import BaseModel
 
 router = APIRouter()
@@ -11,11 +10,9 @@ class UploadInput(BaseModel):
 @router.post("/call_pyrus_upload")
 async def call_pyrus_upload(payload: UploadInput):
     api_url = "https://aimatrix-e8zs.onrender.com/pyrus_mapping"
-    headers = {"Content-Type": "application/json"}
-    payload_json = json.dumps(payload.xmind_url)
-
+    
     try:
-        res = requests.post(api_url, data=payload_json, headers=headers)
+        res = requests.post(api_url, json={"url": payload.xmind_url})
         res.raise_for_status()
         data = res.json()
         return {
