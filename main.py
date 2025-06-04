a29678a9dd93f7592a58e6a25e14aa5f982abf02
Tiffozi-ilia <110051189+Tiffozi-ilia@users.noreply.github.com>
2025-06-04 20:34:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from routes import json_export, md_full, md_clean, zip_export, csv_export, excel_export, opml_export, mm_export, xmind_export, pyrus_upload, xmind_procedures
-from routes.call_pyrus_update import router as call_pyrus_update_router
+from routes.call_pyrus_update import router as call_pyrus_upload_router
 
 app = FastAPI()
 
@@ -15,4 +15,4 @@ app.include_router(mm_export.router)
 app.include_router(xmind_export.router)
 app.include_router(pyrus_upload.router)
 app.include_router(xmind_procedures.router)
-app.include_router(call_pyrus_update_router)
+app.include_router(call_pyrus_upload_router)
