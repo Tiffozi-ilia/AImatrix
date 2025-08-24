9163bd08fa0a2c3122fa5ff269407c7337cec1f8
Tiffozi-ilia <110051189+Tiffozi-ilia@users.noreply.github.com>
2025-08-24 10:08:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routes import json_export, md_full, md_clean, md_clean_cut, md_clean_single, md_clean_depth, md_clean_multiple, zip_export, csv_export, excel_export, opml_export, mm_export, xmind_export, pyrus_upload, xmind_procedures, generate_kpi, xmind_sync_github, update_parent_names, api_ping, download_files
+from routes import json_export, md_full, md_clean, md_clean_cut, md_clean_single, md_clean_depth, md_tree_depth, md_clean_multiple, zip_export, csv_export, excel_export, opml_export, mm_export, xmind_export, pyrus_upload, xmind_procedures, generate_kpi, xmind_sync_github, update_parent_names, api_ping, download_files
 from routes.call_pyrus_upload import router as call_pyrus_upload_router
 
 app = FastAPI()
@@ -10,6 +10,7 @@ app.include_router(md_clean.router)
 app.include_router(md_clean_cut.router)
 app.include_router(md_clean_single.router)
 app.include_router(md_clean_depth.router)
+app.include_router(md_tree_depth.router)
 app.include_router(md_clean_multiple.router)
 app.include_router(zip_export.router)
 app.include_router(csv_export.router)
